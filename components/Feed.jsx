336680dff7391d7ceb7746b6851ed0dd8eaa0490
mrs28 <la.reina.bosque@gmail.com>
2024-02-07 20:22:50 -0500

    feat: components created

---
+++ components/Feed.jsx
@@ -0,0 +1,10 @@
+// here all the prompts
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
---
+++ components/Nav.jsx
@@ -0,0 +1,27 @@
+"use client"; // se marca así porque estoy usando states para interacctividad
+
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  return (
+    <nav className="w-full flex-between mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+      {/* 
+    Desktop Navigation */}
+    </nav>
+  );
+};
+
+export default Nav;
